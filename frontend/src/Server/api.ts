02221d6212177e274ba70224524d051d7a4f49b2
Davi Aragão <omnidavi@gmail.com>
2021-07-20 23:38:12 -0300

    Fix: Updated base url

--- frontend/src/Server/api.ts
+++ frontend/src/Server/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:3333/",
+    baseURL: "https://hackathon-backend01.herokuapp.com/",
     responseType: "json"
 })
